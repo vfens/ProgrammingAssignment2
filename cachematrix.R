8cb752f9156638c74cea8cf23065c1ff43266d55
Vanessa Fens <v_fens@yahoo.com>
2015-10-24 19:10:44 +0200

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## function CACHE
+## function to compute the inverse of a square matrix using a cache
 
-makeCacheMatrix <- function(x = matrix()) {
+## Last updated: October 24, 2015
+## Author: Vanessa Fens
 
+
+# This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(myMatrix = matrix()) {
+        myCachedInverse <- NULL
+        setNonInvertedMatrix <- function(newMatrix) {
+                myMatrix <<- newMatrix
+                myCachedInverse  <<- NULL
+        }
+        getNonInvertedMatrix <- function() myMatrix
+        setInverse <- function(inverseFromCacheSolve) myCachedInverse  <<- inverseFromCacheSolve
+        getInverse <- function() myCachedInverse 
+        
+        # this is the output
+        list(setNonInvertedMatrix = setNonInvertedMatrix, 
+             getNonInvertedMatrix = getNonInvertedMatrix,
+             setInverse = setInverse,
+             getInverse = getInverse)
+      
 }
 
 
-## Write a short comment describing this function
+# This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), 
+# then the cachesolve should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(listOfFunctions, ...) {
+        ## Return the inverse matrix 
+  
+       tempvar <- listOfFunctions$getInverse()  # = myCachedInverse, either NULL or cached inverse
+        if(!is.null(tempvar)) {
+                message("getting cached data")
+                return(tempvar) # returns cached inverse
+                # functie ends here
+        }
+       # no cached inverse yet, so calculate here
+       # first retrieve original matrix
+        data <- listOfFunctions$getNonInvertedMatrix()
+        # calculate inverse
+        tempvar <- solve(data, ...)
+        # insert the cached matrix into the cache!
+        listOfFunctions$setInverse(tempvar)
+        tempvar
 }
+
